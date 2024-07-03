54745af25e4ad2110aece818833f209f7a9b9e72
Simon <kh931623@hotmail.com>
2024-07-03 16:29:52 +0800

    basic login/signup page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Sign In' });
+  res.redirect('/login')
 });
+
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Sign In' });
+})
+
+router.get('/signup', (req, res) => {
+  res.render('signup', { title: 'Sign Up' })
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/login', (req, res) => {
+  const {
+    email,
+    password
+  } = req.body
+
+  res.json({
+    email,
+    password
+  })
+})
+
+router.post('/signup', (req, res) => {
+  const {
+    email,
+    password,
+    passwordConfirm,
+  } = req.body
+
+  res.json({
+    email,
+    password,
+    passwordConfirm,
+  })
+})
 
 module.exports = router;
